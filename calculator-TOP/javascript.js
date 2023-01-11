22edec4aa94b36123e22cd95b826e3d16780e414
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2023-01-11 14:03:26 +0200

    buttons organized operators function added

---
+++ calculator-TOP/javascript.js
@@ -0,0 +1,27 @@
+// const buttons = document.getElementsByTagName('button')
+// console.log(buttons)
+// const h1 = document.createElement('h1')
+
+// buttons.forEach(button => {
+//     h1.appendChild(button)
+    
+// });
+function add(x,y){
+    let result = x+y;
+    return result;
+}
+
+function subtract(x,y){
+    let result = x-y;
+    return result;
+}
+
+function multiply(x,y){
+    let result = x*y;
+    return result;
+}
+
+function divide(x,y){
+    let result = x/y;
+    return result;
+}
